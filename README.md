03b6cde3565989f33a60b0ff86eeee8b5e337861
Marcelo J Forclaz <marceloforclaz@gmail.com>
2020-12-15 17:06:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# WhatsApp actions observer
+## Starter template for WhatsApp chatbot development
+
+--
+
+This template is based on [whatsapp-web.js](https://www.npmjs.com/package/whatsapp-web.js) library.
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs'
+import { Client, ClientSession } from 'whatsapp-web.js'
+import * as qrcode from 'qrcode-terminal'
+
+const SESSION_FILE_PATH: string = './session.json'
+let sessionCfg: ClientSession
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH)
+}
+
+const client: Client = new Client({ session: sessionCfg })
+
+client.initialize()
+
+client.on('qr', (qr) => {
+    // Show QR code on terminal
+    qrcode.generate(qr, { small: true })
+})
+
+client.on('authenticated', (session) => {
+    console.log('Authentication success!')
+    sessionCfg = session
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error('Error while saving session file', err)
+        }
+    })
+})
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessfull
+    console.error('Authentication failure', msg)
+})
+
+client.on('message', async msg => {
+    console.log('Message received', msg)
+
+    if (msg.body == '!ping') {
+        // Send a new message to the same chat
+        client.sendMessage(msg.from, 'pong')
+    }
+})
